d8777c137bb2c0da0ef7536e7bec638138da4cdc
Sai Kumar Balne <balnesaireddy@gmail.com>
2025-03-28 19:22:36 +0000

    rendering user portfolio

--- app.js
+++ app.js
@@ -45,3 +45,32 @@ function populateForm(data) {
     document.querySelector('#city').value = user.city;
     document.querySelector('#email').value = user.email;
 }
+
+// Function to render user portfolio
+function renderPortfolio(user, stocks) {
+    const { portfolio } = user;
+    const portfolioDetails = document.querySelector('.portfolio-list');
+    portfolioDetails.innerHTML = ''; // Clear previous content
+
+    portfolio.forEach(({ symbol, owned }) => {
+        const symbolEl = document.createElement('p');
+        const sharesEl = document.createElement('p');
+        const actionEl = document.createElement('button');
+
+        symbolEl.innerText = symbol;
+        sharesEl.innerText = owned;
+        actionEl.innerText = 'View';
+        actionEl.setAttribute('id', symbol);
+
+        portfolioDetails.appendChild(symbolEl);
+        portfolioDetails.appendChild(sharesEl);
+        portfolioDetails.appendChild(actionEl);
+    });
+
+    // Register event listener on portfolio list
+    portfolioDetails.addEventListener('click', (event) => {
+        if (event.target.tagName === 'BUTTON') {
+            viewStock(event.target.id, stocks);
+        }
+    });
+}
