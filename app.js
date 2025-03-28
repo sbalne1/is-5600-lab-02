3f27d373408e884fb533c2ac3ca3300eab250f1a
Sai Kumar Balne <balnesaireddy@gmail.com>
2025-03-28 19:18:47 +0000

    DOM loaded &Initial rendering of user list

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-/* add your code here */
+document.addEventListener('DOMContentLoaded', () => {
+    const stocksData = JSON.parse(stockContent);
+    const userData = JSON.parse(userContent);
 
+    // Initial rendering of the user list
+    generateUserList(userData, stocksData);
+
+    // Register click event listener on user list
+    document.querySelector('.user-list').addEventListener('click', (event) =>
+        handleUserListClick(event, userData, stocksData)
+    );
+});
